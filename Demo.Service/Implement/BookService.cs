3c677760de59d1ae395e679e2f17c4bdf30a4ef4
haoyu <haoyu.huang@chengyang-info.com.tw>
2023-12-23 11:26:29 +0800

    Demo Service 設定

---
+++ Demo.Service/Implement/BookService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Demo.Repository.Implement;
+using Demo.Repository.Interface;
+using Demo.Repository.Models;
+using Demo.Service.Interface;
+using Demo.Service.Mappings;
+using Demo.Service.Models;
+
+namespace Demo.Service.Implement
+{
+    public class BookService : IBookService
+    {
+        private readonly IMapper _mapper;
+        private readonly IBookRepository _bookRepository;
+
+        public BookService()
+        {
+            var config = new MapperConfiguration(cfg =>
+                cfg.AddProfile<ServiceMappings>());
+
+            this._mapper = config.CreateMapper();
+            this._bookRepository = new BookRepository();
+        }
+
+        /// <summary>
+        /// 查詢資料清單
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<BookResultModel> GetList(BookSearchInfo info)
+        {
+            var condition = this._mapper.Map<BookSearchInfo, BookSearchCondition>(info);
+            var data = this._bookRepository.GetList(condition);
+
+            var result = this._mapper.Map<IEnumerable<BookDataModel>, IEnumerable<BookResultModel>>(data);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 查詢單筆資料
+        /// </summary>
+        /// <param name="id">Book Id</param>
+        /// <returns></returns>   
+        public BookResultModel Get(int id)
+        {
+            var data = this._bookRepository.Get(id);
+            var result = this._mapper.Map<BookDataModel, BookResultModel>(data);
+            return result;
+        }
+
+        /// <summary>
+        /// 新增單筆資料
+        /// </summary>
+        /// <param name="parameter">單筆資料</param>
+        /// <returns></returns>
+        public bool Insert(BookInfo info)
+        {
+            var condition = this._mapper.Map<BookInfo, BookCondition>(info);
+            var result = this._bookRepository.Insert(condition);
+            return result;
+        }
+
+        /// <summary>
+        /// 更新單筆資料
+        /// </summary>
+        /// <param name="id">Book Id</param>
+        /// <param name="parameter">單筆資料</param>
+        /// <returns></returns>
+        public bool Update(int id, BookInfo info)
+        {
+            var condition = this._mapper.Map<BookInfo, BookCondition>(info);
+            var result = this._bookRepository.Update(id, condition);
+            return result;
+        }
+
+        /// <summary>
+        /// 刪除單筆資料
+        /// </summary>
+        /// <param name="id">Book Id</param>
+        /// <returns></returns>
+        public bool Delete(int id)
+        {
+            var result = this._bookRepository.Delete(id);
+            return result;
+        }
+    }
+}
---
+++ Demo.Service/Mappings/ServiceMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Demo.Repository.Models;
+using Demo.Service.Models;
+
+namespace Demo.Service.Mappings
+{
+    public class ServiceMappings : Profile
+    {
+        public ServiceMappings()
+        {
+            this.CreateMap<BookInfo, BookCondition>();
+            this.CreateMap<BookSearchInfo, BookSearchCondition>();
+            this.CreateMap<BookDataModel, BookResultModel>();
+        }
+    }
+}
